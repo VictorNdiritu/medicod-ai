e8dda9ab42e8702765f957c6ee6218c34727ecd2
Victor Ndiritu <87909835+VickyTaar@users.noreply.github.com>
2024-04-23 12:15:22 +0300

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "MediCod AI is an innovative software solution revolutionizing medical coding and billing processes through advanced artificial intelligence. Our mission is to streamline workflows, reduce errors, and ensure compliance, empowering healthcare organizations to enhance efficiency and financial performance.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Automation",
+  "Optimization",
+  "Integration",
+  "Compliance",
+  "Efficiency",
+  "Innovation",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "MediCod AI represents the intersection of healthcare and technology, where innovation meets necessity to drive efficiency and accuracy in medical coding and billing.";
 
 const About = () => {
   return (
